56a64970673ba2929c340f2db29fa3401fcb5ba3
simonkdev <simonkorten09@gmail.com>
2025-06-07 13:53:27 +0200

    rebuilt

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -8,6 +8,12 @@
       name = "nerdfonts droid-sans-mono";
       size = 9;
      };     
+  
+  settings = {
+     "enable_audio_bell" = "no";
+     "background_opacity" = "0.49";
+     "confirm_os_window_close" = 0;
+
 
   };
 
