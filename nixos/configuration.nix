f7daa50ace521ff5a66455a4c4a94022dd5a425b
simonkdev <simonkorten09@gmail.com>
2025-06-09 17:08:10 +0200

    rebuilt system

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -95,10 +95,10 @@
 
  hardware.opengl = {
    enable = true;
-   driSupport = true;
    driSupport32Bit = true;
  };
  
+ hardware.nvidia.open = true;
  services.xserver.videoDrivers = [ "nvidia" ];
  hardware.nvidia.modesetting.enable = true; 
 
