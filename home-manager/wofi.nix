5ca9c77cbaac447ad3181c9ae2162acf8d7556cc
simonkdev <simonkorten09@gmail.com>
2025-06-09 13:09:13 +0200

    rebuilt

--- home-manager/wofi.nix
+++ home-manager/wofi.nix
@@ -9,31 +9,22 @@
 
 window {
 margin: 0px;
-border: 2px solid #a7ffbc;
-background-color: #1e2623;
 border-radius: 12px;
 }
 
 #input {
 margin: 5px;
-border: 0px solid #a7ffbc;
-color: #D3C6AA;
-background-color: #27332e;
 border-radius: 12px;
 }
 
 #inner-box {
 margin: 5px;
 border-radius: 12px;
-border: none;
-background-color: #1e2623;
 }
 
 #outer-box {
 margin: 5px;
-border: none;
 border-radius: 12px;
-background-color: #1e2622;
 }
 
 #scroll {
@@ -44,12 +35,10 @@ border: none;
 #text {
 margin: 5px;
 border: none;
-color: #aad3b2;
 }
 
 #entry:selected {
 border-radius: 12px;
-background-color: #27332f;
 }
 
   '';
