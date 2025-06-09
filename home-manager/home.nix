61c1ec00bbe9e19926a2ba07864138756c660cd0
simonkdev <simonkorten09@gmail.com>
2025-06-09 12:32:53 +0200

    rebuilt

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -119,5 +119,7 @@
   programs.home-manager.enable = true;
   stylix.enable = true;
   stylix.image = ../wallpapers/wallpaper.jpg;
-  stylix.autoEnable = true;
+  stylix.targets = {
+   btop.enable = true;
+  };
 }
