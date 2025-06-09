488a7815c856c792196bf3612b6ca6233944d3b0
simonkdev <simonkorten09@gmail.com>
2025-06-09 13:02:48 +0200

    rebuilt

--- home-manager/wofi.nix
+++ home-manager/wofi.nix
@@ -5,25 +5,45 @@
   programs.wofi = {
    enable = true;
    style = ''
-     window { 
-        margin: 0px;
-	padding: 10px;
-	border-radius: 10px;
-	font-family: JetBrainsMono, monospace;
-  	font-size: 16px;
-     };
+     /* for noobs like me, just copy and paste this into ~/.config/wofi/style.css. that is if you can do that... (no judging m8) */  
+#input {
+margin: 5px;
+border: none;
+color: #d8dee9;
+background-color: #1e1e1e;
+}
 
-    #input {
-	margin: 10px;
-	padding: 6px 10px;
-	border-radius 6px;
-    };
+#inner-box {
+margin: 5px;
+border: none;
+background-color: #1e1e1e; 
+}
 
-    #entry {
-	padding: 6px 10px;
-	margin: 2px 0;
-	border-radius: 6px;
-    };
+#outer-box {
+margin: 5px;
+border: none;
+background-color: #1e1e1e;
+}
+
+#scroll {
+margin: 0px;
+border: 1e1e1e;
+}
+
+#text {
+margin: 5px;
+border: none;
+color: #c4c4c4;
+}
+
+#entry:selected {
+background-color: #1e1e1e;
+}
+/* Add rounded corners to the Wofi window */
+window {
+    border-radius: 10px; /* 10px radius for rounded corners */
+}
+
   '';
   };
 
