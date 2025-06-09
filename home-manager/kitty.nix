677fd5ef823790052a2ffa863af5b03c4b5e003d
simonkdev <simonkorten09@gmail.com>
2025-06-09 11:39:25 +0200

    rebuilt

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -4,10 +4,10 @@
   programs.kitty = {
 
      enable = true;
-    ## font = {
+     font = {
     ##  name = "nerdfonts droid-sans-mono";
-    ##  size = 9;
-    ##  };     
+      size = lib.mkDefault  9;
+      };     
   
   settings = {
      "enable_audio_bell" = "no";
