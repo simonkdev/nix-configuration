16b28de0c76f145c7dc7a0b32807ef453bd2d4db
simonkdev <simonkorten09@gmail.com>
2025-06-18 13:54:11 +0200

    rebuilt system

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -34,7 +34,7 @@
 
   users.users.simonkdev = {
    isNormalUser = true;
-   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+   extraGroups = [ "wheel" "docker"]; # Enable ‘sudo’ for the user.
    packages = with pkgs; [
    tree
    kitty
