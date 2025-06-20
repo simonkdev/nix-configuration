c7196e5cb1ac691cd50e6412ae434f7df595bd33
simonkdev <simonkorten09@gmail.com>
2025-06-20 22:05:27 +0200

    rebuilt system

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,6 +8,7 @@
   nix.settings.experimental-features = [ "nix-command" "flakes"];
 
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnsupportedSystem = true;
   
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
