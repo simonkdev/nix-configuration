6c53d13258af3223d5e368556896d5a0d5538e60
simonkdev <simonkorten09@gmail.com>
2025-06-21 22:54:20 +0200

    rebuilt system

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -165,9 +165,9 @@
    package = config.boot.kernelPackages.nvidiaPackages.stable;
    open = false;
    prime = {
-    sync.enable = false; 
-#    intelBusId = "PCI:0:2:0";
-#    nvidiaBusId = "PCI:1:0:0";
+    sync.enable = true; 
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
    };
 #   dynamicBoost.enable = true;
    nvidiaPersistenced = true;
