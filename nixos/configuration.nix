cf83ba1ba7b1f1f35d36fbb24e76f9a9821fecce
simonkdev <simonkorten09@gmail.com>
2025-06-21 13:46:40 +0200

    rebuilt system

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -120,8 +120,8 @@
    };
  }; 
  
-## hardware.nvidia.open = false;
- services.xserver.videoDrivers = [ "nvidia" ];
+hardware.nvidia.open = true;
+services.xserver.videoDrivers = [ "nvidia" ];
 hardware.nvidia.modesetting.enable = true; 
 hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
 
