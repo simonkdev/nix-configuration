3509c9bbd528ce40a152124693f84ed51e43bc09
simonkdev <simonkorten09@gmail.com>
2025-06-25 14:47:48 +0200

    rebuilt system

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -59,6 +59,7 @@
 
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
  environment.systemPackages = with pkgs; [
    wget
