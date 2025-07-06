73216b79983adb123e45bee9d9610786d4bc6584
simonkdev <simonkorten09@gmail.com>
2025-07-06 16:58:39 +0200

    rebuilt

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -151,7 +151,7 @@
 
   stylix.enable = true;
 #  stylix.image = ../wallpapers/wallpaper.jpg;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-forest.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-heath.yaml";
 ## ashes, 
 
 
