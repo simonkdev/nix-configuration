3416114dbff48f4b3db50c0cfb9e3f1f9a1d76b4
simonkdev <simonkorten09@gmail.com>
2025-07-06 17:07:04 +0200

    rebuilt

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -151,7 +151,7 @@
 
   stylix.enable = true;
 #  stylix.image = ../wallpapers/wallpaper.jpg;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/chalk.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/circus.yaml";
 ## ashes, brewer, catppuccin-mocha
 
 
