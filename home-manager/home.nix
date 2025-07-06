e0892435dd348060b41eb8fd763ea754aaccf7fe
simonkdev <simonkorten09@gmail.com>
2025-07-06 17:17:56 +0200

    rebuilt

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -151,7 +151,7 @@
 
   stylix.enable = true;
 #  stylix.image = ../wallpapers/wallpaper.jpg;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-hard.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-medium.yaml";
 ## ashes, brewer, catppuccin-mocha, darktooth, dracula
 
 
