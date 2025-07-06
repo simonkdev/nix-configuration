32ebc031312d355e247716291020d228b7ad5d5e
simonkdev <simonkorten09@gmail.com>
2025-07-06 16:58:15 +0200

    rebuilt

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -151,7 +151,7 @@
 
   stylix.enable = true;
 #  stylix.image = ../wallpapers/wallpaper.jpg;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-cave.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-forest.yaml";
 ## ashes, 
 
 
