91d98cbbd1b06fb75100b3724f2dd95e9b1e5664
simonkdev <simonkorten09@gmail.com>
2025-07-06 17:12:48 +0200

    rebuilt

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -151,7 +151,7 @@
 
   stylix.enable = true;
 #  stylix.image = ../wallpapers/wallpaper.jpg;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/darkviolet.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/decaf.yaml";
 ## ashes, brewer, catppuccin-mocha, darktooth
 
 
