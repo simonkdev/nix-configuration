3909f2a24bfe8575cf119a0ab67ef5d539cc2678
simonkdev <simonkorten09@gmail.com>
2025-07-11 22:51:18 +0200

    rebuilt

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -9,7 +9,7 @@ programs.waybar = {
             layer = "top";
             modules-left = ["custom/launcher" "cpu" "memory" "custom/weather" "hyprland/workspaces"];
             modules-center = ["mpris" "clock"];
-            modules-right = ["network" "pulseaudio" "tray" "custom/wallpaper" "custom/power-menu"];
+            modules-right = ["network" "pulseaudio" "tray" "custom/power-menu"];
 
           "hyprland/workspaces" = {
             format = "{name}";
