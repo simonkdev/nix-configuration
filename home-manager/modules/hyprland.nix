a37462e7bb33d7a7043baa4c6081a996055f2562
simonkdev <simonkorten09@gmail.com>
2025-07-17 19:48:04 +0200

    rebuilt

--- home-manager/modules/hyprland.nix
+++ home-manager/modules/hyprland.nix
@@ -43,8 +43,8 @@ gestures = {
 monitor = [
 
 "DP-1, preferred, 1920x0@75, 1.0"
-#"eDP-1, preferred, 0x0@75, 1"
- "eDP-1, disable"
+"eDP-1, preferred, 0x0@144, 1"
+ #"eDP-1, disable"
 ];
 
 bezier = [
