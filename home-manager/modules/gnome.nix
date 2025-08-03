14dacde5b8b74bd849976c8b7e3fcd57c0723e7d
simonkdev <simonkorten09@gmail.com>
2025-08-03 19:48:32 +0200

    rebuilt

--- home-manager/modules/gnome.nix
+++ home-manager/modules/gnome.nix
@@ -109,7 +109,7 @@
 
     "org/gnome/desktop/input-sources" = {
       mru-sources = [];
-      sources = lib.mkForce [["('xkb', 'de')"]];
+      sources = lib.mkForce [("xkb" "de")];
       xkb-options = [];
     };
 
