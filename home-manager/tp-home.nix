fd415985a08d1fa80ea2a1dbd6b0867f53dd9520
simonkdev <simonkorten09@gmail.com>
2025-08-04 12:43:30 +0200

    rebuilt

--- home-manager/tp-home.nix
+++ home-manager/tp-home.nix
@@ -10,7 +10,7 @@
    ~/nixsys/home-manager/modules/kitty.nix
    ~/nixsys/home-manager/modules/tp-stylix.nix
    ~/nixsys/home-manager/modules/gnome.nix
-   ~/nixsys/home-manager/modules/nvim.nix
+#   ~/nixsys/home-manager/modules/nvim.nix
   ];
   
   home.stateVersion = "25.05"; # Please read the comment before changing.
