e22dac3c5287a2b075f91e09f368d402eb998de0
simonkdev <simonkorten09@gmail.com>
2025-08-09 21:53:42 +0200

    rebuilt system

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -5,8 +5,14 @@
   boot = {
      
      loader = { 
-     systemd-boot.enable = true;
-     efi.canTouchEfiVariables = true;
+     #systemd-boot.enable = true;
+     #efi.canTouchEfiVariables = true;
+     grub = {
+      device = nodev
+      enable = true;
+      efiSupport = true;
+      useOSProber = true;
+     }
      };
      
      kernelPackages = pkgs.linuxPackages_zen; # Use latest kernel Package
