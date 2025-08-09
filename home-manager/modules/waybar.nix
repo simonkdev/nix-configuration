99ca26b1e12e155931a649d1dc0dd45b543696da
simonkdev <simonkorten09@gmail.com>
2025-08-09 17:17:46 +0200

    rebuilt

--- home-manager/modules/waybar.nix
+++ home-manager/modules/waybar.nix
@@ -11,5 +11,10 @@ home.file."/home/simonkdev/.config/waybar/config" = {
 home.file."/home/simonkdev/.config/waybar/style.css" = {
   source = lib.mkForce /home/simonkdev/nixsys/home-manager/modules/waybar/style.css;
 };
-
+home.file."/home/simonkdev/.config/waybar/machiatto.css" = {
+  source = /home/simonkdev/nixsys/home-manager/modules/waybar/machiatto.css;
+};
+home.file."/home/simonkdev/.config/waybar/style-background.css" = {
+  source = /home/simonkdev/nixsys/home-manager/modules/waybar/style-background.css;
+};
 }
