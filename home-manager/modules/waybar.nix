b85a8440891a9b33a15c9bc4a3a01eafb91c2fa0
simonkdev <simonkorten09@gmail.com>
2025-08-09 17:14:27 +0200

    rebuilt

--- home-manager/modules/waybar.nix
+++ home-manager/modules/waybar.nix
@@ -8,7 +8,4 @@ programs.waybar = {
 home.file."/home/simonkdev/.config/waybar/config" = {
   source = /home/simonkdev/nixsys/home-manager/modules/waybar/config;
 };
-home.file."/home/simonkdev/.config/waybar/style.css" = {
-  source = lib.mkForce /home/simonkdev/nixsys/home-manager/modules/waybar/style.css;
-};
 }
