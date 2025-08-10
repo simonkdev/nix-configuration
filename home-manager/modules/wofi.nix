d8f728f1e3e96ae5d6407b2bacf65ccf3c03d42f
simonkdev <simonkorten09@gmail.com>
2025-08-10 11:48:42 +0200

    rebuilt

--- home-manager/modules/wofi.nix
+++ home-manager/modules/wofi.nix
@@ -6,7 +6,7 @@
    enable = true;
    style = home.file."/home/simonkdev/nixsys/home-manager/modules/style.css";
    config = home.file."/home/simonkdev/nixsys/home-manager/modules/wofi/config";
-    
+  };
 
 }
 	
