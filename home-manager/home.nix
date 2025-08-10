9436fafa91e319c0d953ab235789f3d510dd4423
simonkdev <simonkorten09@gmail.com>
2025-08-10 12:39:55 +0200

    rebuilt

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,14 +1,5 @@
 { config, lib, pkgs, unpkgs, ... }:
-let
-  rapidraw = unpkgs.rapidraw.overrideAttrs (oldAttrs: rec {
-    # After building, override the desktop file Exec command
-    postInstall = oldAttrs.postInstall or "" + ''
-      substituteInPlace $out/share/applications/RapidRAW.desktop --replace \
-        'Exec=rapidraw' \
-        'Exec=GDK_BACKEND=x11 GDK_GL=disable rapidraw'
-    '';
-  });
-in
+
 {
   home.username = "simonkdev";
   home.homeDirectory = "/home/simonkdev";
@@ -16,11 +7,12 @@ in
   ## This directory is ~/nixsys/home-manager :)
 
   imports = [
-   ~/nixsys/home-manager/modules/hyprland.nix
-   ~/nixsys/home-manager/modules/kitty.nix
-   ~/nixsys/home-manager/modules/waybar.nix
-   ~/nixsys/home-manager/modules/wofi/wofi.nix
+   ./modules/hyprland.nix
+   ./modules/kitty.nix
+   ./modules/waybar.nix
+   ./modules/wofi/wofi.nix
    ./modules/stylix.nix
+   ./modules/vscode.nix
   ];
   
   home.stateVersion = "25.05"; # Please read the comment before changing.
@@ -34,10 +26,12 @@ in
     pkgs.playerctl
     pkgs.brightnessctl
     pkgs.upower
-    rapidraw
+    unpkgs.rapidraw
     pkgs.font-awesome
     pkgs.pipes-rs
-        
+
+  ] + [
+
     (pkgs.writeShellScriptBin "homeswitch" ''
         cd ~/nixsys
         git add .
@@ -69,8 +63,6 @@ in
     (pkgs.writeShellScriptBin "waybarconf" ''
        nano /home/simonkdev/nixsys/home-manager/waybar.nix
      '')
-
-
 
   ];
 
@@ -118,43 +110,6 @@ in
    };
   };
 
-  programs.vscode = {
-     enable = true;
-     profiles.default.extensions = with pkgs.vscode-extensions; [
-       catppuccin.catppuccin-vsc
-       catppuccin.catppuccin-vsc-icons
-       esbenp.prettier-vscode
-       vscjava.vscode-java-pack
-       github.copilot-chat
-       github.copilot
-       ms-python.python
-       ms-python.vscode-pylance
-       jnoortheen.nix-ide
-       arrterian.nix-env-selector       
-       jdinhlife.gruvbox 
-       adpyke.codesnap
-       dracula-theme.theme-dracula
-    ];
-    profiles.default.userSettings = {
-       "chat.editor.fontFamily"= "DejaVu Sans Mono";
-       "chat.editor.fontSize"= 12.0;
-       "debug.console.fontFamily"= "DejaVu Sans Mono";
-       "debug.console.fontSize"= 12.0;
-       "editor.fontFamily"= "DejaVu Sans Mono";
-       "editor.fontSize"= 12.0;
-       "editor.inlayHints.fontFamily"= "DejaVu Sans Mono";
-       "editor.inlineSuggest.fontFamily"= "DejaVu Sans Mono";
-       "editor.minimap.sectionHeaderFontSize"= 7.714285714285714;
-       "markdown.preview.fontFamily"= "DejaVu Sans";
-       "markdown.preview.fontSize"= 12.0;
-       "scm.inputFontFamily"= "DejaVu Sans Mono";
-       "scm.inputFontSize"= 11.142857142857142;
-       "screencastMode.fontSize"= 48.0;
-       "terminal.integrated.fontSize" = 12.0;
-       "workbench.colorTheme" = lib.mkForce "Dracula Theme";
-   };
-   };
-     
     programs.yazi = {
        enable = true;
     };
---
+++ home-manager/modules/vscode.nix
@@ -0,0 +1,40 @@
+{lib, config, pkgs, unpkgs, ...}:
+
+{
+programs.vscode = {
+     enable = true;
+     profiles.default.extensions = with pkgs.vscode-extensions; [
+       catppuccin.catppuccin-vsc
+       catppuccin.catppuccin-vsc-icons
+       esbenp.prettier-vscode
+       vscjava.vscode-java-pack
+       github.copilot-chat
+       github.copilot
+       ms-python.python
+       ms-python.vscode-pylance
+       jnoortheen.nix-ide
+       arrterian.nix-env-selector       
+       jdinhlife.gruvbox 
+       adpyke.codesnap
+       dracula-theme.theme-dracula
+    ];
+    profiles.default.userSettings = {
+       "chat.editor.fontFamily"= "DejaVu Sans Mono";
+       "chat.editor.fontSize"= 12.0;
+       "debug.console.fontFamily"= "DejaVu Sans Mono";
+       "debug.console.fontSize"= 12.0;
+       "editor.fontFamily"= "DejaVu Sans Mono";
+       "editor.fontSize"= 12.0;
+       "editor.inlayHints.fontFamily"= "DejaVu Sans Mono";
+       "editor.inlineSuggest.fontFamily"= "DejaVu Sans Mono";
+       "editor.minimap.sectionHeaderFontSize"= 7.714285714285714;
+       "markdown.preview.fontFamily"= "DejaVu Sans";
+       "markdown.preview.fontSize"= 12.0;
+       "scm.inputFontFamily"= "DejaVu Sans Mono";
+       "scm.inputFontSize"= 11.142857142857142;
+       "screencastMode.fontSize"= 48.0;
+       "terminal.integrated.fontSize" = 12.0;
+       "workbench.colorTheme" = lib.mkForce "Dracula Theme";
+   };
+   };
+}
