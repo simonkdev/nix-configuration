6da3d881353f32e0dc6acdfa8cc4f9911615d272
simonkdev <simonkorten09@gmail.com>
2025-08-10 11:52:29 +0200

    rebuilt

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -165,8 +165,8 @@ in
   programs.wofi = {
    enable = true;
   };
-   home.file."/home/simonkdev/.config/wofi/style.css".source = "/home/simonkdev/nixsys/home-manager/modules/style.css";
-   home.file."/home/simonkdev/.config/wofi/config".source = lib.mkForce "/home/simonkdev/nixsys/home-manager/modules/wofi/config";
+   home.file."/home/simonkdev/.config/wofi/style.css".source = lib.mkForce "/home/simonkdev/nixsys/home-manager/modules/style.css";
+   home.file."/home/simonkdev/.config/wofi/config".source = "/home/simonkdev/nixsys/home-manager/modules/wofi/config";
   
 
 }
