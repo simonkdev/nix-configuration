2ee0448ab7c6a28bcc8430be6c08ce059562a1de
simonkdev <simonkorten09@gmail.com>
2025-08-11 14:40:56 +0200

    rebuilt

--- home-manager/modules/devtools/tpcode.nix
+++ home-manager/modules/devtools/tpcode.nix
@@ -1,10 +1,11 @@
 { config, lib, pkgs, ...}:
 
 { 
+  
   options = {
     tpcode.enable = lib.mkEnableOption "enables tpcode";
   };
-  
+
   config = lib.mkIf.config.tpcode.enable {
     programs.vscode.profiles.default.userSettings = {
       "chat.editor.fontSize" = lib.mkForce 16.0;
