7e296f1431942737b3ee9399ac5d0fe8784030b6
simonkdev <simonkorten09@gmail.com>
2025-08-17 20:12:48 +0200

    rebuilt

--- home-manager/modules/stylix/tp-stylix.nix
+++ home-manager/modules/stylix/tp-stylix.nix
@@ -1,37 +1,38 @@
- { config, lib, pkgs, ...}:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  stylix = {
+    enable = true;
+    image = ~/nixsys/wallpapers/wallpaper.png;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    ## ashes, brewer, catppuccin-mocha, darktooth, dracula, gruvbox-material-dark-hard, gruvbox-dark-hard
 
- {
-  stylix = { 
-      enable = true;
-      image = ~/nixsys/wallpapers/scenic-one.jpg;
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-   ## ashes, brewer, catppuccin-mocha, darktooth, dracula, gruvbox-material-dark-hard, gruvbox-dark-hard
-      
-      targets = {
-        btop.enable = true;
-      };
+    targets = {
+      btop.enable = true;
+    };
 
-      cursor = {
-        package = pkgs.bibata-cursors;
-        name = "Bibata-Modern-Ice";
-        size = 16;
-      };
-      
-      fonts.sizes = {
-       applications = 12;
-       terminal = 12;
-       desktop = 12;
-      };
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 16;
+    };
 
+    fonts.sizes = {
+      applications = 12;
+      terminal = 12;
+      desktop = 12;
+    };
   };
 
   gtk = {
-      enable = true;
-       
-      iconTheme = {
+    enable = true;
+
+    iconTheme = {
       name = "Qogir";
       package = pkgs.qogir-icon-theme;
-      };
+    };
   };
-
- }
+}
