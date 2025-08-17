03b0fb285aef4a625a0473d70336dc117c844b90
simonkdev <simonkorten09@gmail.com>
2025-08-17 20:17:49 +0200

    rebuilt

--- home-manager/tp-home.nix
+++ home-manager/tp-home.nix
@@ -83,6 +83,8 @@
   programs.home-manager.enable = true;
   programs.yazi.enable = true;
 
+  home.file."/home/simonkdev/.config/fastfetch/config.jsonc".source = /home/simonkdev/nixsys/home-manager/modules/fastfetch/hypr.jsonc;
+
   home.file."/home/simonkdev/.unison/nas-sync-obsidian.prf".source = /home/simonkdev/nixsys/home-manager/modules/devtools/unison/nas-sync-obsidian.prf;
   home.file."/home/simonkdev/.unison/nas-sync-photos.prf".source = /home/simonkdev/nixsys/home-manager/modules/devtools/unison/nas-sync-photos.prf;
   home.file."/home/simonkdev/.smbcredentials".source = /home/simonkdev/nixsys/home-manager/modules/devtools/unison/.smbcredentials;
