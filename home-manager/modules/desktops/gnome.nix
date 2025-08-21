698067857eab4d2d9ed4ae50ad0501db62983dc2
simonkdev <simonkorten09@gmail.com>
2025-08-21 21:50:01 +0200

    rebuilt

--- home-manager/modules/desktops/gnome.nix
+++ home-manager/modules/desktops/gnome.nix
@@ -44,9 +44,20 @@
         style-scheme = "stylix";
       };
 
+      "org/gnome/calculator" = {
+        base = 10;
+        button-mode = "basic";
+        source-currency = "";
+        source-units = "degree";
+        target-currency = "";
+        target-units = "radian";
+        window-maximized = true;
+        window-size = [360 616];
+      };
+
       "org/gnome/control-center" = {
-        last-panel = "applications";
-        window-state = [980 640 0];
+        last-panel = "privacy";
+        window-state = [980 640 false];
       };
 
       "org/gnome/desktop/a11y/magnifier" = {
@@ -103,6 +114,9 @@
         interval-seconds = 1800;
         play-sound = true;
       };
+      "org/gnome/desktop/calendar" = {
+        show-weekdate = true;
+      };
 
       "org/gnome/desktop/input-sources" = {
         mru-sources = [];
@@ -111,8 +125,9 @@
       };
 
       "org/gnome/desktop/interface" = {
+        clock-show-seconds = true;
         color-scheme = lib.mkForce "prefer-dark";
-        enable-hot-corners = true;
+        enable-hot-corners = false;
         font-name = "DejaVu Sans 12";
         gtk-theme = "adw-gtk3";
         icon-theme = "Qogir";
@@ -123,21 +138,58 @@
       "org/gnome/desktop/notifications" = {
         application-children = ["firefox" "discord" "gnome-power-panel"];
       };
-      "org/gnome/desktop/notifications/application/discord" = {application-id = "discord.desktop";};
-      "org/gnome/desktop/notifications/application/firefox" = {application-id = "firefox.desktop";};
-      "org/gnome/desktop/notifications/application/gnome-power-panel" = {application-id = "gnome-power-panel.desktop";};
-      "org/gnome/desktop/notifications/application/onlyoffice-desktopeditors" = {application-id = "onlyoffice-desktopeditors.desktop";};
-      "org/gnome/desktop/notifications/application/org-gnome-nautilus" = {application-id = "org.gnome.Nautilus.desktop";};
-      "org/gnome/desktop/notifications/application/org-kde-kdenlive" = {application-id = "org.kde.kdenlive.desktop";};
-      "org/gnome/desktop/notifications/application/spotify" = {application-id = "spotify.desktop";};
-      "org/gnome/desktop/notifications/application/thunar" = {application-id = "thunar.desktop";};
 
-      "org/gnome/desktop/peripherals/mouse" = {accel-profile = "flat";};
-      "org/gnome/desktop/peripherals/touchpad" = {two-finger-scrolling-enabled = true;};
-      "org/gnome/desktop/screensaver" = {lock-enabled = false;};
+      "org/gnome/desktop/notifications/application/altus" = {
+        application-id = "Altus.desktop";
+      };
+      "org/gnome/desktop/notifications/application/discord" = {
+        application-id = "discord.desktop";
+      };
+      "org/gnome/desktop/notifications/application/firefox" = {
+        application-id = "firefox.desktop";
+      };
+      "org/gnome/desktop/notifications/application/gnome-power-panel" = {
+        application-id = "gnome-power-panel.desktop";
+      };
+      "org/gnome/desktop/notifications/application/onlyoffice-desktopeditors" = {
+        application-id = "onlyoffice-desktopeditors.desktop";
+      };
+      "org/gnome/desktop/notifications/application/org-gnome-nautilus" = {
+        application-id = "org.gnome.Nautilus.desktop";
+      };
+      "org/gnome/desktop/notifications/application/org-kde-kdenlive" = {
+        application-id = "org.kde.kdenlive.desktop";
+      };
+      "org/gnome/desktop/notifications/application/spotify" = {
+        application-id = "spotify.desktop";
+      };
+      "org/gnome/desktop/notifications/application/thunar" = {
+        application-id = "thunar.desktop";
+      };
 
-      "org/gnome/desktop/search-providers" = {sort-order = ["org.gnome.Settings.desktop" "org.gnome.Contacts.desktop" "org.gnome.Nautilus.desktop"];};
-      "org/gnome/desktop/session" = {idle-delay = 600;};
+      "org/gnome/desktop/peripherals/mouse" = {
+        accel-profile = "flat";
+      };
+
+      "org/gnome/desktop/peripherals/touchpad" = {
+        two-finger-scrolling-enabled = true;
+      };
+
+      "org/gnome/desktop/screensaver" = {
+        lock-enabled = false;
+      };
+
+      "org/gnome/desktop/search-providers" = {
+        sort-order = [
+          "org.gnome.Settings.desktop"
+          "org.gnome.Contacts.desktop"
+          "org.gnome.Nautilus.desktop"
+        ];
+      };
+
+      "org/gnome/desktop/session" = {
+        idle-delay = 600;
+      };
 
       "org/gnome/desktop/wm/keybindings" = {
         close = ["<Super>c"];
@@ -212,6 +264,7 @@
       "org/gnome/nautilus/preferences" = {
         default-folder-viewer = "list-view";
         migrated-gtk-settings = true;
+        open-folder-on-dnd-hover = false;
         search-filter-time-type = "last_modified";
       };
       "org/gnome/nautilus/window-state" = {
@@ -307,7 +360,7 @@
           ["yazi.desktop"]
         ];
         command-history = ["thunar" "vsc"];
-        disabled-extensions = ["dock-from-dash@fthx"];
+        disabled-extensions = ["dock-from-dash@fthx" "advanced-weather@sanjai.com"];
         enabled-extensions = [
           "user-theme@gnome-shell-extensions.gcampax.github.com"
           "blur-my-shell@aunetx"
@@ -315,6 +368,10 @@
           "lockscreen-extension@pratap.fastmail.fm"
           "search-light@icedman.github.com"
           "tiling-assistant@leleat-on-github"
+          "gtk4-ding@smedius.gitlab.com"
+          "editdesktopfiles@dannflower"
+          "add-to-desktop@tommimon.github.com"
+          "azclock@azclock.gitlab.com"
         ];
         favorite-apps = [
           "firefox.desktop"
@@ -333,6 +390,225 @@
         welcome-dialog-last-shown-version = "48.1";
       };
       "org/gnome/shell/app-switcher" = {current-workspace-only = false;};
+      "org/gnome/shell/extensions/advanced-weather" = {
+        fill-button-background = true;
+        location-mode = "auto";
+        panel-position = "left";
+        show-location-label = false;
+        use-12hour-format = true;
+      };
+
+      "org/gnome/shell/extensions/azclock" = {
+        create-initial-widget = false;
+        prefs-visible-page = "";
+        update-notifier-project-version = 14;
+        widgets = [
+          {"38824b70-97c3-42fb-8be5-2171cdd0d4dd" = {enabled = true;};}
+        ];
+      };
+
+      "org/gnome/shell/extensions/azclock/widget-data/38824b70-97c3-42fb-8be5-2171cdd0d4dd" = {
+        anchor-point = "Top_Right";
+        background-color = "rgba(0, 0, 0, .6)";
+        elements = [
+          {"d4d1e99f-fe00-4520-bcf2-1c6e1e63b434" = {enabled = true;};}
+        ];
+        location = [1920 48];
+        name = "Weather Widget";
+        show-background = false;
+        show-border = false;
+      };
+
+      "org/gnome/shell/extensions/azclock/widget-data/38824b70-97c3-42fb-8be5-2171cdd0d4dd/element-data/d4d1e99f-fe00-4520-bcf2-1c6e1e63b434" = {
+        current-weather-icon-type = "Full_Color";
+        element-type = "Weather_Element";
+        # locations is a complex GVariant, you may need to use lib.hm.gvariant.mkVariant or similar for full fidelity
+        name = "Weather Forecast";
+        polling-interval = 300;
+        show-current-apparent-temp = true;
+        show-daily-forecast = false;
+        show-location = false;
+        # weather-actor-shadow is a tuple, use a list for simple cases
+        weather-actor-shadow = [false "rgba(0, 0, 0, 0.4)" 0 2 0 4];
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell" = {
+        pipelines = {
+          pipeline_default = {
+            name = "Default";
+            effects = [
+              {
+                type = "native_static_gaussian_blur";
+                id = "effect_000000000000";
+                params = {
+                  radius = 30;
+                  brightness = 0.74;
+                  unscaled_radius = 14;
+                };
+              }
+            ];
+          };
+          pipeline_default_rounded = {
+            name = "Default rounded";
+            effects = [
+              {
+                type = "native_static_gaussian_blur";
+                id = "effect_000000000001";
+                params = {
+                  radius = 30;
+                  brightness = 0.6;
+                };
+              }
+              {
+                type = "corner";
+                id = "effect_000000000002";
+                params = {
+                  radius = 24;
+                };
+              }
+            ];
+          };
+        };
+        settings-version = 2;
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell/appfolder" = {
+        brightness = 0.6;
+        sigma = 30;
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell/coverflow-alt-tab" = {
+        pipeline = "pipeline_default";
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell/dash-to-dock" = {
+        blur = true;
+        brightness = 0.6;
+        pipeline = "pipeline_default_rounded";
+        sigma = 30;
+        static-blur = true;
+        style-dash-to-dock = 0;
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell/lockscreen" = {
+        pipeline = "pipeline_default";
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell/overview" = {
+        pipeline = "pipeline_default";
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell/panel" = {
+        brightness = 0.6;
+        pipeline = "pipeline_default";
+        sigma = 30;
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell/screenshot" = {
+        pipeline = "pipeline_default";
+      };
+
+      "org/gnome/shell/extensions/blur-my-shell/window-list" = {
+        brightness = 0.6;
+        sigma = 30;
+      };
+
+      "org/gnome/shell/extensions/dash-to-dock" = {
+        background-opacity = 0.8;
+        dash-max-icon-size = 48;
+        dock-position = "BOTTOM";
+        height-fraction = 0.9;
+        hot-keys = false;
+        multi-monitor = true;
+        preferred-monitor = -2;
+        preferred-monitor-by-connector = "DP-1";
+      };
+
+      "org/gnome/shell/extensions/gtk4-ding" = {
+        icon-size = "standard";
+        show-home = false;
+        show-trash = false;
+        show-volumes = false;
+      };
+
+      "org/gnome/shell/extensions/openbar" = {
+        bg-change = true;
+        bguri = "file:///nix/store/2xdmgicyzf5cx94z43p7jzszzijfyh9x-wallpaper.png";
+        count1 = 239916;
+        count2 = 62422;
+        count3 = 60145;
+        count4 = 57918;
+        count5 = 42260;
+        count6 = 26322;
+        count7 = 19440;
+        count8 = 7061;
+        count9 = 2669;
+        count10 = 2441;
+        count11 = 6;
+        count12 = 2;
+        dark-bguri = "file:///nix/store/2xdmgicyzf5cx94z43p7jzszzijfyh9x-wallpaper.png";
+        dark-palette1 = ["9" "27" "32"];
+        dark-palette2 = ["62" "129" "194"];
+        dark-palette3 = ["35" "67" "86"];
+        dark-palette4 = ["101" "180" "241"];
+        dark-palette5 = ["40" "82" "126"];
+        dark-palette6 = ["79" "109" "132"];
+        dark-palette7 = ["188" "191" "202"];
+        dark-palette8 = ["95" "61" "58"];
+        dark-palette9 = ["184" "71" "67"];
+        dark-palette10 = ["44" "68" "65"];
+        dark-palette11 = ["20" "76" "164"];
+        dark-palette12 = ["128" "164" "116"];
+        default-font = "Sans 12";
+        import-export = false;
+        light-bguri = "file:///nix/store/2xdmgicyzf5cx94z43p7jzszzijfyh9x-wallpaper.png";
+        light-palette1 = ["9" "27" "32"];
+        light-palette2 = ["62" "129" "194"];
+        light-palette3 = ["35" "67" "86"];
+        light-palette4 = ["101" "180" "241"];
+        light-palette5 = ["40" "82" "126"];
+        light-palette6 = ["79" "109" "132"];
+        light-palette7 = ["188" "191" "202"];
+        light-palette8 = ["95" "61" "58"];
+        light-palette9 = ["184" "71" "67"];
+        light-palette10 = ["44" "68" "65"];
+        light-palette11 = ["20" "76" "164"];
+        light-palette12 = ["128" "164" "116"];
+        monitor-height = 1080;
+        monitor-width = 1920;
+        palette1 = ["9" "27" "32"];
+        palette2 = ["62" "129" "194"];
+        palette3 = ["35" "67" "86"];
+        palette4 = ["101" "180" "241"];
+        palette5 = ["40" "82" "126"];
+        palette6 = ["79" "109" "132"];
+        palette7 = ["188" "191" "202"];
+        palette8 = ["95" "61" "58"];
+        palette9 = ["184" "71" "67"];
+        palette10 = ["44" "68" "65"];
+        palette11 = ["20" "76" "164"];
+        palette12 = ["128" "164" "116"];
+        pause-reload = false;
+        reloadstyle = false;
+      };
+
+      "org/gnome/shell/extensions/search-light" = {
+        animation-speed = 100.0;
+        blur-brightness = 0.6;
+        blur-sigma = 30.0;
+        entry-font-size = 1;
+        monitor-count = 1;
+        preferred-monitor = 0;
+        scale-height = 0.1;
+        scale-width = 0.1;
+        shortcut-search = ["<Super>space"];
+      };
+
+      "org/gnome/shell/extensions/tiling-assistant" = {
+        focus-hint-color = "rgb(53,132,228)";
+        last-version-installed = 52;
+        overridden-settings = {"org.gnome.mutter.edge-tiling" = false;};
+      };
 
       "org/gnome/shell/extensions/user-theme" = {
         name = "Stylix";
