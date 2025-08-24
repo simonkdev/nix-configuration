3d251397c3f9cd335fb440f80b733f05c1598bfb
simonkdev <simonkorten09@gmail.com>
2025-08-24 20:07:30 +0200

    rebuilt

--- home-manager/tp-home.nix
+++ home-manager/tp-home.nix
@@ -70,7 +70,7 @@
       sudo sshfs webadmin@46.252.195.82:/home/webadmin/FleetManager ~/Documents/02-BSED/fleetmanager -o allow_other
     '')
     (pkgs.writeShellScriptBin "sshman" ''
-      /home/simonkdev/nixsys/home-manager/modules/ssh_manager/hlbm
+      /home/simonkdev/nixsys/home-manager/modules/devtools/ssh_manager/hlbm
     '')
   ];
 
