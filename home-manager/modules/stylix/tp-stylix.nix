604362645d8cafb784ba8cf21d916a7f0234b1ee
simonkdev <simonkorten09@gmail.com>
2025-09-11 20:01:17 +0200

    rebuilt

--- home-manager/modules/stylix/tp-stylix.nix
+++ home-manager/modules/stylix/tp-stylix.nix
@@ -7,7 +7,7 @@
   stylix = {
     enable = true;
     image = ~/nixsys/wallpapers/wallpaper.png;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-hard.yaml";
     ## ashes, brewer, catppuccin-mocha, darktooth, dracula, gruvbox-material-dark-hard, gruvbox-dark-hard
 
     targets = {
