ee53656e91ab45bfd57449d45c59ee6e8b3242b5
simonkdev <simonkorten09@gmail.com>
2025-09-13 16:13:07 +0200

    rebuilt

--- home-manager/modules/stylix/tp-stylix.nix
+++ home-manager/modules/stylix/tp-stylix.nix
@@ -6,7 +6,7 @@
 }: {
   stylix = {
     enable = true;
-    image = ~/nixsys/wallpapers/gruv-porsche.png;
+    image = ~/nixsys/wallpapers/wallp-lake.png;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-hard.yaml";
     ## ashes, brewer, catppuccin-mocha, darktooth, dracula, gruvbox-material-dark-hard, gruvbox-dark-hard
 
