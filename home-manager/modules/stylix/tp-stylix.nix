1245c3f2bd7c3c2b005c6ba49403680c14de5e52
simonkdev <simonkorten09@gmail.com>
2025-10-28 09:31:40 +0100

    rebuilt

--- home-manager/modules/stylix/tp-stylix.nix
+++ home-manager/modules/stylix/tp-stylix.nix
@@ -7,7 +7,7 @@
   stylix = {
     enable = true;
     image = ~/nixsys/wallpapers/switch-uw.png;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-hard.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/black-metal.yaml";
     ## ashes, brewer, catppuccin-mocha, darktooth, dracula, gruvbox-material-dark-hard, gruvbox-dark-hard
 
     targets = {
