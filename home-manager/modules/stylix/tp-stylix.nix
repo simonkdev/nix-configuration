cd0ccf25abf3c2848e9dc039adf52a463f569fcf
simonkdev <simonkorten09@gmail.com>
2025-10-28 11:06:51 +0100

    rebuilt

--- home-manager/modules/stylix/tp-stylix.nix
+++ home-manager/modules/stylix/tp-stylix.nix
@@ -7,7 +7,7 @@
   ## bathory, gorgoroth, khold, mayhem, immortal
   stylix = {
     enable = true;
-    image = ~/nixsys/wallpapers/switch-uw.png;
+    image = ~/nixsys/wallpapers/garage-ultrawide.png;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/black-metal-khold.yaml";
     ## ashes, brewer, catppuccin-mocha, darktooth, dracula, gruvbox-material-dark-hard, gruvbox-dark-hard
 
