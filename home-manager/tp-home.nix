05c5fbfe0903ab00ccf4cf9f40208b9dc7cee49b
simonkdev <simonkorten09@gmail.com>
2026-01-14 20:51:19 +0100

    rebuilt

--- home-manager/tp-home.nix
+++ home-manager/tp-home.nix
@@ -104,6 +104,33 @@
   programs.yazi.enable = true;
 
   programs.hyprlock.enable = true;
+  programs.hyprlock.settings = {
+    input-field = {
+      monitor = "";
+      size = [250 60];
+      outline_thickness = 2;
+
+      dots_size = 0.2;
+      dots_spacing = 0.35;
+      dots_center = true;
+
+      outer_color = "rgba(0, 0, 0, 0)";
+      inner_color = "rgba(0, 0, 0, 0.2)";
+      font_color = "$foreground";
+
+      fade_on_empty = false;
+      rounding = -1;
+
+      check_color = "rgb(204, 136, 34)";
+      placeholder_text = "<i><span foreground=\"##cdd6f4\">Input Password...</span></i>";
+      hide_input = false;
+
+      position = [0 (-200)];
+      halign = "center";
+      valign = "center";
+    };
+  };
+
   programs.hyprlock.extraConfig = ''
 
           # Background with blur
