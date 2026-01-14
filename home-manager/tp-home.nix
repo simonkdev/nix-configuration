c538c1cbb6ae36c39821c13aef8410d37f5ba68a
simonkdev <simonkorten09@gmail.com>
2026-01-14 20:52:29 +0100

    rebuilt

--- home-manager/tp-home.nix
+++ home-manager/tp-home.nix
@@ -114,14 +114,14 @@
       dots_spacing = 0.35;
       dots_center = true;
 
-      outer_color = "rgba(0, 0, 0, 0)";
-      inner_color = "rgba(0, 0, 0, 0.2)";
-      font_color = "$foreground";
+      #outer_color = "rgba(0, 0, 0, 0)";
+      #inner_color = "rgba(0, 0, 0, 0.2)";
+      #font_color = "$foreground";
 
       fade_on_empty = false;
       rounding = -1;
 
-      check_color = lib.mkForce "rgb(204, 136, 34)";
+      #check_color = lib.mkForce "rgb(204, 136, 34)";
       placeholder_text = "<i><span foreground=\"##cdd6f4\">Input Password...</span></i>";
       hide_input = false;
 
