4aca57028480e41854832a3455c717125ee487a4
simonkdev <simonkorten09@gmail.com>
2026-01-21 16:00:48 +0100

    rebuilt

--- home-manager/modules/desktops/hyprland.nix
+++ home-manager/modules/desktops/hyprland.nix
@@ -124,7 +124,7 @@
         "$mod, S, exec, discord"
         "$mod, X, exec, librewolf"
         "$mod, A, exec, steam"
-        ", Print, exec, hyprshot -m output -m DP-1 -o ~/screenshots"
+        "$mod, #, exec, hyprshot -m output -m DP-1 -o ~/screenshots"
         "$mod, P, exec, GDK_BACKEND=x11 GDK_GL=disable rapidraw"
         "$mod, Y, exec, hyprlock"
 
