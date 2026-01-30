4c60088ddbca12c801b057f212c3e3e3bfff0882
simonkdev <simonkorten09@gmail.com>
2026-01-31 00:26:33 +0100

    rebuilt

--- home-manager/modules/stylix/tp-stylix.nix
+++ home-manager/modules/stylix/tp-stylix.nix
@@ -7,7 +7,8 @@
   ## bathory, gorgoroth, khold, mayhem, immortal
   stylix = {
     enable = true;
-    image = ~/nixsys/wallpapers/porsche-tokyo-uw.png;
+    # absolute path avoids Nix treating it like a source derivation
+    image = "/home/simonkdev/nixsys/wallpapers/porsche-tokyo-uw.png";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     ## ashes, brewer, catppuccin-mocha, darktooth, dracula, gruvbox-material-dark-hard, gruvbox-dark-hard, black-metal-khold
 
@@ -35,15 +36,17 @@
   gtk = {
     enable = true;
 
-    theme = lib.mkForce {
-      name = lib.mkForce "WhiteSur-dark";
-      package = lib.mkForce pkgs.whitesur-gtk-theme;
+    # Avoid forcing evaluation of the package/name; use plain values so Nix doesn't try
+    # to treat them as source archives during evaluation.
+    theme = {
+      name = "WhiteSur-dark";
+      package = pkgs.whitesur-gtk-theme;
       ## quogir + pkgs.qogir-gtk-theme
     };
 
     iconTheme = {
       name = lib.mkForce "WhiteSur";
-      package = lib.mkForce pkgs.whitesur-icon-theme;
+      package = lib.mkForce pkgs.whitesur-icon-theme.purple;
       #package = lib.mkForce pkgs.cosmic-icons;
       ## quogir + pkgs.qogir-icon-theme
     };
