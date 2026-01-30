d2c707e79d42f70c00923075fb435c9618f73fe6
simonkdev <simonkorten09@gmail.com>
2026-01-31 00:54:12 +0100

    rebuilt

--- home-manager/modules/stylix/tp-stylix.nix
+++ home-manager/modules/stylix/tp-stylix.nix
@@ -9,7 +9,7 @@
     enable = true;
 
     # Use an existing wallpaper file so ImageMagick can open it during builds
-    image = "/home/simonkdev/nixsys/wallpapers/wallpaper.jpg";
+    image = "/home/simonkdev/nixsys/wallpapers/porsche-tokyo-uw.png";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     ## ashes, brewer, catppuccin-mocha, darktooth, dracula, gruvbox-material-dark-hard, gruvbox-dark-hard, black-metal-khold
 
