aebaa76bbc3fb0fa601616dc22570b481af24f54
simonkdev <simonkorten09@gmail.com>
2026-04-23 20:45:55 +0200

    rebuilt

--- home-manager/modules/wofi/wofi.nix
+++ home-manager/modules/wofi/wofi.nix
@@ -7,112 +7,144 @@
   programs.wofi = {
     enable = true;
     style = ''
-          @keyframes fadeIn {
-          0% {
-          }
-          100% {
-          }
-      }
+      @keyframes fadeIn {
+           0% { opacity: 0; }
+           100% { opacity: 1; }
+         }
 
-      * {
-          all:unset;
-          font-family: 'CodeNewRoman Nerd Font Mono', monospace;
-          font-size: 18px;
-          outline: none;
-          border: none;
-          text-shadow:none;
-          background-color: ${config.lib.stylix.colors.withHashtag.base00};
-      }
+         * {
+           all: unset;
+           font-family: 'CodeNewRoman Nerd Font Mono', monospace;
+           font-size: 18px;
+           outline: none;
+           border: none;
+           text-shadow: none;
+           background-color: ${config.lib.stylix.colors.withHashtag.base00};
+         }
 
-      window {
-          background-color: alpha(${config.lib.stylix.colors.withHashtag.base08}, 1);
-          color: ${config.lib.stylix.colors.withHashtag.base0B};
-          all:unset;
-          padding: 0px;
-          border-radius: 4px;
-      }
-      #inner-box {
-          margin: 2px;
-          padding: 10px
-          border: none;
-      }
-      #outer-box {
-          border: none;
-      }
-      #scroll {
-          margin: 0px;
-          padding: 0px;
-          border: none;
-      }
-      #input {
-          all:unset;
-          margin-left:20px;
-          margin-right:20px;
-          margin-top:20px;
-          padding: 20px;
-          border: 2px solid ${config.lib.stylix.colors.withHashtag.base01};
-          color: ${config.lib.stylix.colors.withHashtag.base0C};
-          outline: none;
-          box-shadow: 1px 1px 5px rgba(0,0,0, .5);
-          border-radius:10;
-          background-color: alpha(${config.lib.stylix.colors.withHashtag.base01}, 0.2);
-      }
-      #input image {
-          border: none;
-          color: ${config.lib.stylix.colors.withHashtag.base09};
-          padding-right:10px;
-      }
-      #input * {
-          border: none;
-          outline: none;
-          color: ${config.lib.stylix.colors.withHashtag.base0C};
-      }
+         window {
+           background-color: alpha(${config.lib.stylix.colors.withHashtag.base08}, 1);
+           color: ${config.lib.stylix.colors.withHashtag.base0B};
+           all: unset;
+           padding: 0px;
+           border-radius: 4px;
+         }
 
-      #input:focus {
-          outline: none;
-          border: none;
+         #inner-box {
+           margin: 2px;
+           padding: 10px;
+           border: none;
+         }
 
-          border-radius:10;
-      }
-      #text {
-          margin: 5px;
-          border: none;
-          outline: none;
-          color: ${config.lib.stylix.colors.withHashtag.base05};
-      }
-      #text {
-          margin: 5px;
-          border: none;
-          outline: none;
-          color: ${config.lib.stylix.colors.withHashtag.base05};
-      }
-      #entry {
-          border: none;
-          margin: 5px;
-          padding: 0px;
-          color: ${config.lib.stylix.colors.withHashtag.base06};
-      }
-      #entry arrow {
-          border: none;
-          color: ${config.lib.stylix.colors.withHashtag.base08};
-      }
-      #entry:selected {
-          box-shadow: 1px 1px 5px rgba(255,255,255, .03);
-          border: 2px solid ${config.lib.stylix.colors.withHashtag.base0C};
-          border-radius: 0px;
-          background-color:transparent;
-      }
-      #entry:selected #text {
-          color: ${config.lib.stylix.colors.withHashtag.base0C};
-      }
-      #entry:drop(active) {
-          background-color: ${config.lib.stylix.colors.withHashtag.base0C};
-      }
-      #entry:nth-child(even),
-      #entry:nth-child(odd) {
-      background-color: transparent;
-      }
-    '';
+         #outer-box {
+           border: none;
+         }
+
+         #scroll {
+           margin: 0px;
+           padding: 0px;
+           border: none;
+         }
+
+         #input {
+           all: unset;
+           margin-left: 20px;
+           margin-right: 20px;
+           margin-top: 20px;
+           padding: 20px;
+           border: 2px solid ${config.lib.stylix.colors.withHashtag.base01};
+           color: ${config.lib.stylix.colors.withHashtag.base0C};
+           outline: none;
+           box-shadow: 1px 1px 5px rgba(0,0,0,.5);
+           border-radius: 10px;
+           background-color: alpha(${config.lib.stylix.colors.withHashtag.base01}, 0.2);
+         }
+
+         #input image {
+           border: none;
+           color: ${config.lib.stylix.colors.withHashtag.base09};
+           padding-right: 10px;
+         }
+
+         #input * {
+           border: none;
+           outline: none;
+           color: ${config.lib.stylix.colors.withHashtag.base0C};
+         }
+
+         #input:focus {
+           outline: none;
+           border: none;
+           border-radius: 10px;
+         }
+
+         #text {
+           margin: 5px;
+           border: none;
+           outline: none;
+           color: ${config.lib.stylix.colors.withHashtag.base05};
+         }
+
+         #entry {
+           border: none;
+           margin: 5px;
+           padding: 0px;
+           color: ${config.lib.stylix.colors.withHashtag.base06};
+         }
+
+         #entry arrow {
+           border: none;
+           color: ${config.lib.stylix.colors.withHashtag.base08};
+         }
+
+         #entry:selected {
+           box-shadow: 1px 1px 5px rgba(255,255,255,.03);
+           border: 2px solid ${config.lib.stylix.colors.withHashtag.base0C};
+           border-radius: 0px;
+           background-color: transparent;
+         }
+
+         #entry:selected #text {
+           color: ${config.lib.stylix.colors.withHashtag.base0C};
+         }
+
+         #entry:drop(active) {
+           background-color: ${config.lib.stylix.colors.withHashtag.base0C};
+         }
+
+         #entry:nth-child(even),
+         #entry:nth-child(odd) {
+           background-color: transparent;
+         }
+
+         /* FRBFStudios/rofi-specific styling */
+         element {
+           background-color: ${config.lib.stylix.colors.withHashtag.base00};
+           color: ${config.lib.stylix.colors.withHashtag.base05};
+         }
+
+         element selected {
+           background-color: ${config.lib.stylix.colors.withHashtag.base0A};
+           color: ${config.lib.stylix.colors.withHashtag.base00};
+           padding: 0 8px;
+         }
+
+         element urgent {
+           background-color: ${config.lib.stylix.colors.withHashtag.base08};
+           color: ${config.lib.stylix.colors.withHashtag.base00};
+         }
+
+         mode-switcher {
+           padding: 0 8px;
+         }
+
+         prompt {
+           padding: 0 8px;
+         }
+
+         entry {
+           padding: 0 8px;
+         }   '';
   };
   home.file."/home/simonkdev/.config/wofi/config".source = "/home/simonkdev/nixsys/home-manager/modules/wofi/config";
 }
