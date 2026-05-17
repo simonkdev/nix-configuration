cbc8caa41edb7e8f637aab8fdbb06757cbe90db8
simonkdev <simonkorten09@gmail.com>
2026-05-17 19:28:44 +0200

    rebuilt system

--- nixos/modules/devtools/networking.nix
+++ nixos/modules/devtools/networking.nix
@@ -1,15 +1,15 @@
-{ config, lib, pkgs, ... }:
-
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   networking = {
-  
-  hostName = "nixPad"; # Define your hostname.
-  wireless.enable = false;  # Enables wireless support via wpa_supplicant.
-  networkmanager.enable = true; 
-  
+    hostName = "nixPad"; # Define your hostname.
+    wireless.enable = false; # Enables wireless support via wpa_supplicant.
+    networkmanager.enable = true;
   };
+  hardware.enableRedistributableFirmware = true;
 
   services.openssh.enable = true; # Enable SSH
-
-
 }
