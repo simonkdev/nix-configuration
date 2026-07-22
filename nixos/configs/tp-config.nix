354c7df5da722e053e6139064f131651e8363d09
simonkdev <simonkorten09@gmail.com>
2026-07-22 23:16:07 +0200

    rebuilt system

--- nixos/configs/tp-config.nix
+++ nixos/configs/tp-config.nix
@@ -10,7 +10,7 @@
 let
   sddm-astronaut =
     (pkgs.sddm-astronaut.override {
-      embeddedTheme = "black_hole"; # or any other theme
+      embeddedTheme = "pixel_sakura_static"; # or any other theme
       themeConfig = {
         # Customize colors and settings
         HeaderTextColor = "${config.lib.stylix.colors.base0A}";
