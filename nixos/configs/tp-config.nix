094c0a812ade4d8e9b06601d8fe764929318d51c
simonkdev <simonkorten09@gmail.com>
2026-07-22 22:59:50 +0200

    rebuilt system

--- nixos/configs/tp-config.nix
+++ nixos/configs/tp-config.nix
@@ -12,7 +12,7 @@
     themeConfig = {
       # Customize colors and settings
       HeaderTextColor = "#d5c4a1";
-      Background = "Backgrounds/your-custom-background.png";
+      Background = "Backgrounds/obi-wan-vs-vader-uw.jpg";
       # ... other theme configuration options
     };
   }).overrideAttrs (oldAttrs: {
@@ -118,25 +118,6 @@ in
       };
   };
 
-  systemd.services.applyUserMonitorSettings =
-    let
-      username = "simonkdev";
-      gdmConfigDir = "/var/lib/gdm/seat0/config";
-    in
-    {
-      description = "Apply user monitor settings to GDM login screen";
-      after = [
-        "network.target"
-        "systemd-user-sessions.service"
-        "display-manager.service"
-      ];
-      wantedBy = [ "multi-user.target" ];
-
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = "${pkgs.bash}/bin/bash -c 'mkdir -p ${gdmConfigDir} && [ \"/home/${username}/.config/monitors.xml\" -ef \"${gdmConfigDir}/monitors.xml\" ] || cp /home/${username}/.config/monitors.xml ${gdmConfigDir}/monitors.xml && chown gdm:gdm ${gdmConfigDir}/monitors.xml'";
-      };
-    };
 
   services.desktopManager.plasma6.enable = true;
 
