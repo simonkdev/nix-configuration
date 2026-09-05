7a88a58152833d0bfb3f65f95c215722d8709f7f
simonkdev <simonkorten09@gmail.com>
2026-09-06 00:56:11 +0200

    rebuilt

--- home-manager/tp-home.nix
+++ home-manager/tp-home.nix
@@ -143,7 +143,7 @@
     GDK_SCALE = "1";
     QT_SCALE_FACTOR = "1";
     GTK_THEME = "breeze";
-    QT_STYLE_OVERRIDE = "";
+    QT_STYLE_OVERRIDE = lib.mkForce "";
   };
 
   programs.btop.enable = true;
