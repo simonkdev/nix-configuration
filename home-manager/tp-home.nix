e4a2c4b0e7a22a5366651c6f8f1591edb7e79a0f
simonkdev <simonkorten09@gmail.com>
2026-09-06 11:11:22 +0200

    rebuilt

--- home-manager/tp-home.nix
+++ home-manager/tp-home.nix
@@ -143,8 +143,11 @@
     GDK_SCALE = "1";
     QT_SCALE_FACTOR = "1";
     GTK_THEME = "breeze";
-    QT_STYLE_OVERRIDE = lib.mkForce "breeze";
   };
+
+  home.sessionVariablesExtra = ''
+    unset QT_STYLE_OVERRIDE
+  '';
 
   home.file.".config/systemd/user/plasma-plasmashell.service.d/override.conf".text = ''
     [Service]
